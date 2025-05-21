55ea923b88c1f8edf5bc8c8ea5fef1d0fdbdaacf
domcoast <domcoastn8n@gmail.com>
2025-05-21 18:25:31 +0530

    Update generate-image.js

--- functions/generate-image.js
+++ functions/generate-image.js
@@ -1,4 +1,4 @@
-const { createCanvas, loadImage, registerFont } = require('canvas');
+const { createCanvas, loadImage } = require('@napi-rs/canvas');
 const path = require('path');
 const fs = require('fs');
 
