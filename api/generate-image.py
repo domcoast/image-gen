98a62877faab2dd57dcba5b50664e4be9212a249
domcoast <domcoastn8n@gmail.com>
2025-05-22 14:46:32 +0530

    Update generate-image.py

--- api/generate-image.py
+++ api/generate-image.py
@@ -29,8 +29,8 @@ def generate_image():
 
     # Load fonts with absolute paths
     try:
-        font_path_regular = os.path.join(os.path.dirname(__file__), 'arial.ttf')
-        font_path_bold = os.path.join(os.path.dirname(__file__), 'arialbd.ttf')
+        font_path_regular = os.path.join(os.path.dirname(__file__), '../arial.ttf')
+        font_path_bold = os.path.join(os.path.dirname(__file__), '../arialbd.ttf')
         font_regular = ImageFont.truetype(font_path_regular, 16)
         font_bold = ImageFont.truetype(font_path_bold, 24)
     except Exception as e:
